11710e17f3867769f6c5d37160da887b98d6f686
Ivan Toropygin <itori86@mail.ru>
2024-04-10 21:03:18 +0300

    update project

--- app/src/main/java/com/example/cryptoapp/domain/GetCoinInfoListUseCase.kt
+++ app/src/main/java/com/example/cryptoapp/domain/GetCoinInfoListUseCase.kt
@@ -3,6 +3,5 @@ package com.example.cryptoapp.domain
 class GetCoinInfoListUseCase(
     private val repository: CoinRepository
 ) {
-
     operator fun invoke() = repository.getCoinInfoList()
 }
--- app/src/main/java/com/example/cryptoapp/domain/GetCoinInfoUseCase.kt
+++ app/src/main/java/com/example/cryptoapp/domain/GetCoinInfoUseCase.kt
@@ -3,6 +3,5 @@ package com.example.cryptoapp.domain
 class GetCoinInfoUseCase(
     private val repository: CoinRepository
 ) {
-
     operator fun invoke(fromSymbol: String) = repository.getCoinInfo(fromSymbol)
 }
