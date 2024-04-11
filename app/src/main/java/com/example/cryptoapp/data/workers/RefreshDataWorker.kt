ba24434f9f77553a86871228462e4411a33ca8e9
Ivan Toropygin <itori86@mail.ru>
2024-04-11 22:10:17 +0300

    add RefreshDataWorkerFactory

--- app/src/main/java/com/example/cryptoapp/data/workers/RefreshDataWorker.kt
+++ app/src/main/java/com/example/cryptoapp/data/workers/RefreshDataWorker.kt
@@ -4,18 +4,19 @@ import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkerParameters
-import com.example.cryptoapp.data.database.AppDatabase
+import com.example.cryptoapp.data.database.CoinInfoDao
 import com.example.cryptoapp.data.mapper.CoinMapper
-import com.example.cryptoapp.data.network.ApiFactory
+import com.example.cryptoapp.data.network.ApiService
 import kotlinx.coroutines.delay
 
-class RefreshDataWorker(context: Context, workerParameters: WorkerParameters) :
-    CoroutineWorker(context, workerParameters) {
+class RefreshDataWorker(
+    context: Context,
+    workerParameters: WorkerParameters,
+    private val coinInfoDao: CoinInfoDao,
+    private val apiService: ApiService,
+    private val mapper: CoinMapper
+) : CoroutineWorker(context, workerParameters) {
 
-    private val coinInfoDao = AppDatabase.getInstance(context).coinPriceInfoDao()
-    private val apiService = ApiFactory.apiService
-
-    private val mapper = CoinMapper()
     override suspend fun doWork(): Result {
         while (true) {
             try {
@@ -25,10 +26,9 @@ class RefreshDataWorker(context: Context, workerParameters: WorkerParameters) :
                 val coinInfoDtoList = mapper.mapJsonContainerToListCoinInfo(jsonContainer)
                 val dbModelList = coinInfoDtoList.map { mapper.mapDtoToDbModel(it) }
                 coinInfoDao.insertPriceList(dbModelList)
-
             } catch (e: Exception) {
             }
-            delay(10_000)
+            delay(10000)
         }
     }
 
